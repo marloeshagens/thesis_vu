c36ffce59a1bd20ef4b2d45e60ceb73df65d1520
Marloes Hagens <marloeshagens@hotmail.com>
2021-05-31 09:25:13 +0000

    apply SC on EUTL data + cleaner code

--- code_EUTL_v1.R
+++ code_EUTL_v1.R
@@ -6,9 +6,7 @@ rm(list = ls())
 ################################################################################  
 ############################### 0. LOAD PACKAGES ############################### 
 ################################################################################ 
-
-####### install packages
-#######
+####### install packages #######
 # install.packages("Synth")
 # install.packages("ArCo")
 # install.packages("xlsx")
@@ -22,6 +20,7 @@ rm(list = ls())
 # install.packages("tidyverse") #includes readxl function
 # install.packages(glmnet) #contains LASSO function (Friedman et al., 2010).
 
+####### load packages #######
 library(xlsx)
 library(ggplot2)
 library(moments)
@@ -39,20 +38,21 @@ library(glmnet) #contains LASSO function (Friedman et al., 2010).
 ############################ 1. LOAD & PREPARE DATA ############################
 ################################################################################  
 
-#1.1. emissions data (EU)
+####1.1. emissions data (EU)####
 
 #select only the power installations
 compliance_data <- read.csv("compliance.csv", header = TRUE)
 installation_data <- read.csv("installation.csv", header = TRUE)
 joined_df <- merge(compliance_data, installation_data, by.x = "installation_id", 
                    by.y = "id", all.x = TRUE, all.y = FALSE)
-df <- subset(joined_df, activity_id == 1 | activity_id == 20) 
+df <- subset(joined_df, activity_id == 1 | activity_id == 20) # only activity 1 and 20; power installations
 
 #split & select United Kingdom and rest of EU data set 
 df_GB <- subset(df, country_id == "GB")
 df_exclGB <- subset(df, country_id != "GB")
 
-# 1.2. data for predictors used for the SC method
+#### 1.2. data for predictors used for the SC method ####
+
 # 1.2.1 coal-to-gas price ratio
 
 # coal prices
@@ -70,8 +70,8 @@ df_exclGB <- subset(df, country_id != "GB")
 ############################### 2. DESCRIPTIVES ################################ 
 ################################################################################  
 
-# descriptives & plots 
-# whole EU
+#### descriptives and plots #######
+## the whole EU
 df_aggregate <- aggregate(df$verified, by=list(year=df$year), FUN=sum, na.rm=TRUE, na.action=NULL)
 df_aggregate <- df_aggregate[-c(16:26), ]
 # augmented dickey-fueller test - stationarity
@@ -83,7 +83,7 @@ plot(ts(df_aggregate$x, start = 2005 , end = 2019),
         ylim=c(800000000, 1600000000),
         xlim=c(2005,2019))
 
-# GB
+## Great Britain
 df_GB_aggregate <- aggregate(df_GB$verified, by=list(year=df_GB$year), FUN=sum, na.rm=TRUE, na.action=NULL)
 df_GB_aggregate <- df_GB_aggregate[-c(16), ]
 
@@ -95,7 +95,7 @@ plot(ts(df_GB_aggregate$x, start = 2005 , end = 2019),
      xlim=c(2005,2019))
 abline(v=2013, col="grey", lty=2, lwd=2)
 
-# rest of EU
+## rest of the EU
 df_exclGB_aggregate <- aggregate(df_exclGB$verified, by=list(year=df_exclGB$year), FUN=sum, na.rm=TRUE, na.action=NULL)
 df_exclGB_aggregate <- df_exclGB_aggregate[-c(16:26), ]
 
@@ -106,7 +106,8 @@ plot(ts(df_exclGB_aggregate$x, start = 2005 , end = 2019),
      #ylim=c(800000000, 1600000000),
      xlim=c(2005,2019))
 
-# normalize function, such we can compare the data to each
+#### Normalize function to compare the data ####
+# normalizing function, such we can compare the data to each
 normalize <- function(x) {
         return ((x - min(x)) / (max(x) - min(x))) 
         }
@@ -134,7 +135,9 @@ legend("topright",
        cex=0.8
 )
 
-# ADF test (null = unit root) & KPSS test (null = trend stationarity)
+#### ADF & KPSS tests ####
+## ADF test (null = unit root)
+## KPSS test (null = trend stationarity)
 
 # ADF & KPSS - whole EU
 adf.test(df_aggregate$x) # the data set has a unit root. 
@@ -148,8 +151,7 @@ kpss.test(df_GB_aggregate$x, null = "Trend") # the data has a unit root, trend s
 adf.test(df_exclGB_aggregate$x) # the data set has a unit root at alpha = 0.05
 kpss.test(df_exclGB_aggregate$x, null = "Trend") # data has a unit at alpha = 0.10
 
-# detrending
-
+#### Detrending ####
 # detrending - EU
 df_diff<- diff(df_aggregate$x)
 adf.test(df_diff) # the data is stationary at alpha=0.10
@@ -192,13 +194,13 @@ plot(ts(df_diff_exclGB, start = 2005 , end = 2019),
      #ylim=c(800000000, 1600000000),
      xlim=c(2005,2019))
 
-
 ################################################################################  
 ################################### 3. ARCO #################################### 
 ################################################################################  
 
-###### 3.1. ArCo ######
-# ArCo is fitted using linear regression functions below
+##### 3.1. ArCo Linear Regressions #####
+# ArCo can be fitted using linear regression functions below
+# note: in the application below, we use LASSO, not this linear regression.
 fn=function(X,y){
         return(lm(y~X))
 }
@@ -206,6 +208,7 @@ p.fn=function(model,newdata){
         b=coef(model)
         return(cbind(1,newdata) %*% b)}
 
+##### 3.2. Arco data prep ####
 # data prep: UK (treated) and other EU countries (peers) in 1 df 
 df2 <- aggregate(verified ~year+country_id, data=df, FUN=sum, na.rm=TRUE, na.action = NULL)
 
@@ -225,9 +228,9 @@ t0 <- 9 #t0: year of implementation
 N <- ncol(df2)
 n_peers <- N-1 # nr of peers
 
-# appropriate input in ArCo fit
-data <- list(df2)
+data <- list(df2) # appropriate input in ArCo fit
 
+#### 3.3 ArCo application ####
 # approximation of t0 - does not work 
 t0_hat=estimate_t0(data = data, fn = fn, p.fn = p.fn, treated.unit = 1) 
 
@@ -243,7 +246,65 @@ arco2 <- fitArCo(data = data, fn = cv.glmnet, p.fn = predict, treated.unit = 1 ,
 plot(arco2,plot=1,display.fitted = TRUE)
 # does not look good either + gives a warning message
 
+################################################################################  
+################################### 4. SYNTH ################################### 
+################################################################################  
 
-################################################################################  
-################################### 3. SYNTH ################################### 
-################################################################################  
+#### 4.1 SC data prep ####
+dfLong <- melt(df2, value.name="verified_emissions") #Var1=year, Var2=Country_code
+colnames(dfLong) <- c("year", "country_code","verified_emissions")
+dfLong <- transform(dfLong, unit.id=as.numeric(factor(country_code)))
+dfLong$year <- as.numeric(dfLong$year) # time.variable must be a numeric variable
+dfLong$country_code <- as.character(dfLong$country_code) # unit.names.variable must be a character variable
+dfLong$verified_emissions <- as.numeric(dfLong$verified_emissions) # predictor must be a numeric variable
+
+SC_dataprep.out<- dataprep(
+                foo = dfLong,
+                predictors = "verified_emissions",
+                predictors.op = "mean", #mean is the default
+                dependent = "verified_emissions",
+                unit.variable = "unit.id",  # this has to be a numeric variable
+                time.variable = "year",
+                treatment.identifier = 1, #united kingdom
+                controls.identifier = c(2:32), # you have to specify at least two control units
+                time.predictors.prior = c(2005:2012),
+                time.optimize.ssr = c(2005:2013), 
+                unit.names.variable = "country_code", 
+                time.plot = 2005:2019
+                )
+
+#### 4.2 apply SC ####
+synth.out <- synth(SC_dataprep.out)
+
+## summarizing the results by accessing the output from synth.out directly
+round(synth.out$solution.w,3) # contains the unit weights
+synth.out$solution.v  ## contains the predictor weights. 
+# MH: here I only have 1 predictor so weight = 1
+
+## the period by period discrepancies between the treated unit and its SC unit
+gaps<- SC_dataprep.out$Y1plot-(
+        SC_dataprep.out$Y0plot%*%synth.out$solution.w)
+
+# Get result tables - V and W weights plus balance btw. treated and SC
+synth.tables <- synth.tab(
+        dataprep.res = SC_dataprep.out,
+        synth.res = synth.out)
+print(synth.tables)
+
+## summary plots for outcome trajectories of the treated and the SC unit
+
+#### 4.2 plot SC ####
+
+## plot in levels (path; treated and synthetic)
+path.plot(dataprep.res = SC_dataprep.out, 
+          synth.res = synth.out,
+          Ylab = c("Verified CO2 emissions"),
+          Xlab = c("Year"), 
+          #Ylim = c(0,13), 
+          Legend = c("UK","Synthetic UK"),
+) 
+## plot the gaps (treated - synthetic)
+gaps.plot(dataprep.res = SC_dataprep.out,
+          synth.res = synth.out,
+          Ylab = c("Gap in Verified CO2 emissions (MtCO2e)"),
+          Xlab = c("Year"))
