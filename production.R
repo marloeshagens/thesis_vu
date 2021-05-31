9b4e832988524c1fa790e0aa3c27ad6cc3a89215
Marloes Hagens <marloeshagens@hotmail.com>
2021-05-31 09:25:47 +0000

    fixed data input for the SC

--- production.R
+++ production.R
@@ -97,36 +97,38 @@ p2<alpha #TRUE
 
 ###### SC ######
 # prepare data for SC
-df <- df[-c(2)]
+df <- df[-c(2)] # remove SIEC column
 dfLong <- melt(df, id.vars="TIME", 
-               variable.name = "GEO", value.name="GEP (TJ)")
-
-# create a numeric unit variable to indicate which country is which "unit.num"
+               variable.name = "GEO", value.name="GEP") #transform to a long df
 N <- ncol(df)-1 # N = nr of countries, incl treated unit.
 T <- nrow(df) # T = nr of Years
-dfLong$unit.num <- rep(c(1:T),times=N)
-x<-rep(c(1),times=T)
+dfLong$year.id <- rep(c(1:T),times=N)
+dfLong <- transform(dfLong, unit.id=as.numeric(factor(GEO)))
+
+dfLong$TIME <- as.numeric(dfLong$TIME) # time.variable must be a numeric variable
+dfLong$GEO <- as.character(dfLong$GEO) # unit.names.variable must be a character variable
+dfLong$GEP <- as.numeric(dfLong$GEP) # predictor must be a numeric variable
 
 # create matrices from panel data that provide inputs for synth()
 dataprep.out<-
   dataprep(
     foo = dfLong,
-    predictors = "MtCo2e",
+    predictors = "GEP",
     predictors.op = "mean", #mean is the default
-    dependent = "MtCo2e",
-    unit.variable = "unit.num",  # this has to be a numeric variable
-    time.variable = "Year",
+    dependent = "GEP",
+    unit.variable = "unit.id",  # this has to be a numeric variable
+    time.variable = "TIME",
     # not sure what the below command is for.
     # special.predictors = list( 
     #  list("Y", 1991, "mean"),
     # list("Y", 1985, "mean"),
     # list("Y", 1980, "mean")
     # ),
-    treatment.identifier = 29, #united kingdom
-    controls.identifier = c(1:28), # you have to specify at least two control units
+    treatment.identifier = 33, #united kingdom
+    controls.identifier = c(4:32), # you have to specify at least two control units
     time.predictors.prior = c(1990:2012),
     time.optimize.ssr = c(1990:2013), 
-    unit.names.variable = "Country", 
+    unit.names.variable = "GEO", 
     time.plot = 1990:2018
   )
 
@@ -154,7 +156,7 @@ print(synth.tables)
 ## plot in levels (path; treated and synthetic)
 path.plot(dataprep.res = dataprep.out, 
           synth.res = synth.out,
-          Ylab = c("CO2 emissions (MtCO2e)"),
+          Ylab = c("GEP (TJ)"),
           Xlab = c("Year"), 
           #Ylim = c(0,13), 
           Legend = c("UK","Synthetic UK"),
@@ -162,5 +164,5 @@ path.plot(dataprep.res = dataprep.out,
 ## plot the gaps (treated - synthetic)
 gaps.plot(dataprep.res = dataprep.out,
           synth.res = synth.out,
-          Ylab = c("Gap in CO2 emissions (MtCO2e)"),
+          Ylab = c("Gap in GEP (TJ)"),
           Xlab = c("Year"))
